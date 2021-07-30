f9fa5a803980809a28cc0cebc59f9ac87af8f36b
WilsonW <wilson20ti@mahasiswa.pcr.ac.id>
2021-07-30 16:52:25 +0700

    Models & Controllers, -Views

---
+++ controllers/proses.php
@@ -0,0 +1,110 @@
+<?php
+include '../models/database.php';
+$db = new database();
+
+if ($_GET['aksi'] == "register_user") {
+    if ($_POST['password'] == $_POST['password_confirmation']) {
+        $db->input_user($_POST['username'], $_POST['password'], 'user');
+        $_SESSION['name'] = $_POST['username'];
+        $_SESSION['akses'] = 'user';
+        header("location:../views/user.php");
+    } else {
+        header("location:../views/register.php");
+    }
+} elseif ($_GET['aksi'] == "login") {
+    $cek = $db->login($_POST['username'], $_POST['password'], 'admin');
+    if ($cek != 0) {
+        $_SESSION['akses'] = 'admin';
+        header("location:../views/admin.php");
+    } else {
+        $cek = $db->login($_POST['username'], $_POST['password'], 'user');
+        if ($cek != 0) {
+            $_SESSION['akses'] = 'user';
+            header("location:../views/user.php");
+        } else {
+            header("location:../views");
+        }
+    }
+} elseif ($_GET['aksi'] == "logout") {
+    session_start();
+    session_destroy();
+    header('location:../views');
+}
+#User (Login)
+elseif ($_GET['aksi'] == "tambah_user") {
+    $db->input_user($_POST['username'], $_POST['password'], $_POST['akses']);
+    header("location:../views/user.php");
+} elseif ($_GET['aksi'] == "delete_user") {
+    $db->delete_user($_GET['id']);
+    header("location:../views/user.php");
+} elseif ($_GET['aksi'] == "update_user") {
+    $db->update_user($_POST['username'], $_POST['password'], $_POST['akses']);
+    header("location:../views/user.php");
+}
+#Customer
+elseif ($_GET['aksi'] == "tambah_customer") {
+    $db->input_customer($_POST['kode_customer'], $_POST['nama_customer'], $_POST['kota_customer'], $_POST['alamat_customer'], $_POST['telp_customer']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "delete_customer") {
+    $db->delete_customer($_GET['kode_customer']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "update_customer") {
+    $db->update_customer($_POST['kode_customer'], $_POST['nama_customer'], $_POST['kota_customer'], $_POST['alamat_customer'], $_POST['telp_customer']);
+    header("location:../views/admin.php");
+}
+#Furniture
+elseif ($_GET['aksi'] == "tambah_furniture") {
+    $db->input_furniture($_POST['kode_furniture'], $_POST['nama_furniture'], $_POST['jenis_furniture'], $_POST['bahan_furniture'], $_POST['stok_furniture'], $_POST['harga_furniture']);
+    if ($_SESSION['akses'] = 'user') {
+        header("location:../views/user.php");
+    } elseif ($_SESSION['akses'] = 'admin') {
+        header("location:../views/admin.php");
+    }
+} elseif ($_GET['aksi'] == "delete_furniture") {
+    $db->delete_furniture($_GET['kode_furniture']);
+    if ($_SESSION['akses'] = 'user') {
+        header("location:../views/user.php");
+    } elseif ($_SESSION['akses'] = 'admin') {
+        header("location:../views/admin.php");
+    }
+} elseif ($_GET['aksi'] == "update_furniture") {
+    $db->update_furniture($_POST['kode_furniture'], $_POST['nama_furniture'], $_POST['jenis_furniture'], $_POST['bahan_furniture'], $_POST['stok_furniture'], $_POST['harga_furniture']);
+    if ($_SESSION['akses'] = 'user') {
+        header("location:../views/user.php");
+    } elseif ($_SESSION['akses'] = 'admin') {
+        header("location:../views/admin.php");
+    }
+}
+#Supplier
+elseif ($_GET['aksi'] == "tambah_supplier") {
+    $db->input_supplier($_POST['kode_supplier'], $_POST['nama_supplier'], $_POST['kota_supplier'], $_POST['alamat_supplier'], $_POST['telp_supplier']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "delete_supplier") {
+    $db->delete_supplier($_GET['kode_supplier']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "update_supplier") {
+    $db->update_supplier($_POST['kode_supplier'], $_POST['nama_supplier'], $_POST['kota_supplier'], $_POST['alamat_supplier'], $_POST['telp_supplier']);
+    header("location:../views/admin.php");
+}
+#Pembelian
+elseif ($_GET['aksi'] == "tambah_pembelian") {
+    $db->input_pembelian($_POST['kode_beli'], $_POST['kode_furniture'], $_POST['kode_customer'], $_POST['tgl_beli'], $_POST['jml_beli'], $_POST['harga_beli']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "delete_pembelian") {
+    $db->delete_pembelian($_GET['kode_beli']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "update_pembelian") {
+    $db->update_pembelian($_POST['kode_beli'], $_POST['kode_furniture'], $_POST['kode_customer'], $_POST['tgl_beli'], $_POST['jml_beli'], $_POST['harga_beli']);
+    header("location:../views/admin.php");
+}
+#Penjualan
+elseif ($_GET['aksi'] == "tambah_penjualan") {
+    $db->input_penjualan($_POST['kode_jual'], $_POST['kode_furniture'], $_POST['kode_supplier'], $_POST['tgl_jual'], $_POST['jml_jual'], $_POST['harga_jual']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "delete_penjualan") {
+    $db->delete_penjualan($_GET['kode_jual']);
+    header("location:../views/admin.php");
+} elseif ($_GET['aksi'] == "update_penjualan") {
+    $db->update_penjualan($_POST['kode_jual'], $_POST['kode_furniture'], $_POST['kode_supplier'], $_POST['tgl_jual'], $_POST['jml_jual'], $_POST['harga_jual']);
+    header("location:../views/admin.php");
+}
---
+++ models/database.php
@@ -0,0 +1,302 @@
+<?php
+class database
+{
+
+    var $host = "localhost";
+    var $username = "root";
+    var $pass = "";
+    var $db = "furniturekel7_";
+    var $con;
+
+    function __construct()
+    {
+        $this->con = mysqli_connect($this->host, $this->username, $this->pass, $this->db);
+        mysqli_select_db($this->con, $this->db);
+        session_start();
+    }
+
+    function login($name_user, $password, $akses)
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * FROM login where username='$name_user' AND password='$password' AND akses='$akses'"
+        );
+        $_SESSION['name'] = $name_user;
+        while ($d = mysqli_fetch_row($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    #User (Login)
+    function tampil_data_user()
+    {
+        $data = mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "SELECT * from login");
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function input_user($name_user, $password, $akses)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "INSERT INTO user (username, password, akses) 
+		 VALUES ('$name_user', '$password', '$akses')");
+    }
+
+    function delete_user($name_user)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "DELETE from login where username='$name_user'");
+    }
+
+    function edit_user($name_user)
+    {
+        $data = mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "SELECT username, password, akses FROM user where username='$name_user'");
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function update_user($name_user, $password, $akses)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "UPDATE login SET username = '$name_user'
+		, password='$password', akses = '$akses' WHERE username = '$name_user'");
+    }
+
+
+    #Customer
+    function tampil_data_customer()
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * from customer"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function input_customer($kode_customer, $nama_customer, $kota_customer, $alamat_customer, $telp_customer)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "INSERT into customer(kode_customer, nama_customer, kota_customer, alamat_customer, telp_customer) VALUES ('$kode_customer, $nama_customer, $kota_customer, $alamat_customer, $telp_customer')"
+        );
+    }
+
+    function delete_customer($kode_customer)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "DELETE from customer where kode_customer='$kode_customer'"
+        );
+    }
+
+    function edit_customer($kode_customer)
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * FROM customer where kode_customer='$kode_customer'"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+    function update_customer($kode_customer, $nama_customer, $kota_customer, $alamat_customer, $telp_customer)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "UPDATE customer
+		SET kode_customer= '$kode_customer', nama_customer = '$nama_customer', kota_customer = '$kota_customer' 
+		,alamat_customer = '$alamat_customer', telp_customer = '$telp_customer' WHERE kode_customer = '$kode_customer'");
+    }
+
+    #Furniture
+    function tampil_data_furniture()
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * from furniture"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function input_furniture($kode_furniture, $nama_furniture, $jenis_furniture, $bahan_furniture, $stok_furniture, $harga_furniture)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "INSERT into furniture(kode_furniture, nama_furniture, jenis_furniture, bahan_furniture, stok_furniture, harga_furniture) VALUES ('$kode_furniture, $nama_furniture, $jenis_furniture, $bahan_furniture, $stok_furniture, $harga_furniture')"
+        );
+    }
+
+    function delete_furniture($kode_furniture)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "DELETE from furniture where kode_furniture='$kode_furniture'"
+        );
+    }
+
+    function edit_furniture($kode_furniture)
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * FROM furniture where kode_furniture='$kode_furniture'"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+    function update_furniture($kode_furniture, $nama_furniture, $jenis_furniture, $bahan_furniture, $stok_furniture, $harga_furniture)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "UPDATE furniture
+            SET kode_furniture=$kode_furniture, nama_furniture = '$nama_furniture', jenis_furniture = '$jenis_furniture' 
+            ,bahan_furniture = '$bahan_furniture', stok_furniture = '$stok_furniture', harga_furniture = '$harga_furniture' WHERE kode_furniture = '$kode_furniture'");
+    }
+
+    #Supplier
+    function tampil_data_supplier()
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * from supplier"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function input_supplier($kode_supplier, $nama_supplier, $kota_supplier, $alamat_supplier, $telp_supplier)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "INSERT into supplier(kode_supplier, nama_supplier, kota_supplier, alamat_supplier, telp_supplier) VALUES ('$kode_supplier, $nama_supplier, $kota_supplier, $alamat_supplier, $telp_supplier')"
+        );
+    }
+
+    function delete_supplier($kode_supplier)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "DELETE from supplier where kode_supplier='$kode_supplier'"
+        );
+    }
+
+    function edit_supplier($kode_supplier)
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * FROM supplier where kode_supplier='$kode_supplier'"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+    function update_supplier($kode_supplier, $nama_supplier, $kota_supplier, $alamat_supplier, $telp_supplier)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "UPDATE supplier
+		SET kode_supplier= '$kode_supplier', nama_supplier = '$nama_supplier', kota_supplier = '$kota_supplier' 
+		,alamat_supplier = '$alamat_supplier', telp_supplier = '$telp_supplier' WHERE kode_supplier = '$kode_supplier'");
+    }
+
+    #Pembelian
+    function tampil_data_pembelian()
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT kode_beli, f.kode_furniture, c.kode_customer, tgl_beli, jml_beli, harga_beli from pembelian, furniture f, customer c"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function input_pembelian($kode_beli, $kode_furniture, $kode_customer, $tgl_beli, $jml_beli, $harga_beli)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "INSERT into pembelian(kode_beli, kode_furniture, kode_customer, tgl_beli, jml_beli, harga_beli) VALUES ('$kode_beli, $kode_furniture, $kode_customer, $tgl_beli, $jml_beli, $harga_beli')"
+        );
+    }
+
+    function delete_pembelian($kode_beli)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "DELETE from pembelian where kode_beli='$kode_beli'"
+        );
+    }
+
+    function edit_pembelian($kode_beli)
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * FROM pembelian where kode_beli='$kode_beli'"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+    function update_pembelian($kode_beli, $kode_furniture, $kode_customer, $tgl_beli, $jml_beli, $harga_beli)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "UPDATE pembelian
+            SET kode_beli= '$kode_beli', kode_furniture = '$kode_furniture', , kode_customer = '$kode_customer', tgl_beli = '$tgl_beli' 
+            ,jml_beli = '$jml_beli', harga_beli = '$harga_beli' WHERE kode_beli = '$kode_beli'");
+    }
+
+    #Penjualan
+    function tampil_data_penjualan()
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT kode_jual, f.kode_furniture, s.kode_supplier, tgl_jual, jml_jual, harga_jual from penjualan, furniture f, supplier s"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function input_penjualan($kode_jual, $kode_furniture, $kode_supplier, $tgl_jual, $jml_jual, $harga_jual)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "INSERT into penjualan(kode_jual, kode_furniture, kode_supplierr, tgl_jual, jml_jual, harga_jual) VALUES ('$kode_jual, $kode_furniture, $kode_supplier, $tgl_jual, $jml_jual, $harga_jual')"
+        );
+    }
+
+    function delete_penjualan($kode_jual)
+    {
+        mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "DELETE from penjualan where kode_jual='$kode_jual'"
+        );
+    }
+
+    function edit_penjualan($kode_jual)
+    {
+        $data = mysqli_query(
+            mysqli_connect($this->host, $this->username, $this->pass, $this->db),
+            "SELECT * FROM penjualan where kode_jual='$kode_jual'"
+        );
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+    function update_penjualan($kode_jual, $kode_furniture, $kode_supplier, $tgl_jual, $jml_jual, $harga_jual)
+    {
+        mysqli_query(mysqli_connect($this->host, $this->username, $this->pass, $this->db), "UPDATE penjualan
+            SET kode_jual= '$kode_jual', kode_furniture = '$kode_furniture', , kode_supplier = '$kode_supplier', tgl_jual = '$tgl_jual' 
+            ,jml_jual = '$jml_jual', harga_jual = '$harga_jual' WHERE kode_jual = '$kode_jual'");
+    }
+}
